2d1d728a97b5029fb57955d0bcd8c10f1efcea4c
dejan <dsindjelic@live.com>
2024-04-16 13:36:50 +0200

    console app with CommandLineParser nuget pckg, to connect to Elastic Search and insert or search, based on the optional letter

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,56 @@
-﻿namespace esclient;
+﻿using CommandLine;
+using Nest;
+
+
+namespace esclient;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Try to parsing");
+        Parser.Default.ParseArguments<Es1>(args)
+            .WithParsed(opts =>
+            {
+                Console.WriteLine("Try to connect to ES");
+                var settings = new ConnectionSettings(new Uri("http://localhost:9200"))
+                .DefaultIndex("test_index")
+                .BasicAuthentication(opts.username, opts.password);
+
+                var client = new ElasticClient(settings);
+
+                Console.WriteLine("Connected!");
+
+                // Indeksiranje dokumenta
+                Console.WriteLine("Try to indexing or searching");
+                Console.WriteLine($"Name is {opts.name}");
+                if (opts.name != null)
+                {
+                    if (opts.insert)
+                    {
+                        var response = client.IndexDocument(new { Name = opts.name, Age = opts.age });
+                        Console.WriteLine("Indexed");
+                    }
+                    else if (opts.search)
+                    {
+                        Console.WriteLine($"Option is {opts.search} I'll try to search");
+
+                        var searchResponse = client.Search<Person>(s => s
+                        .Query(q => q
+                            .Match(m => m
+                                .Field(f => f.Name)
+                                    .Query(opts.name)
+                                    )));
+
+                        Console.WriteLine(("Results"));
+
+                        foreach (var hit in searchResponse.Hits)
+                        {
+                            Console.WriteLine($"Name> {hit.Source.Name} --- Age:{hit.Source.Age} --- Index:{hit.Index} --- ES Id:{hit.Id}");
+                        }
+                    }
+                }
+            })
+            .WithNotParsed(errs => Console.WriteLine(errs.ToString()));
     }
 }

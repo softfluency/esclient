3170462a8eb81efe0fa01f8543d0477c2cd86591
MiloBg <milofcrs@gmail.com>
2024-04-19 15:03:16 +0200

    Cleaned code and added logic for checking ES status (not working)

--- src/EsOptions.cs
+++ src/EsOptions.cs
@@ -1,22 +1,24 @@
 ﻿using CommandLine;
 
-namespace esclient
+namespace esclient;
+
+public class EsOptions
 {
-    public class EsOptions
-    {
-        [Option('u', "username", Required = true, HelpText = "Username")]
-        public string? Username { get; set; }
+    [Option('u', "username", Required = true, HelpText = "Username")]
+    public string? Username { get; set; }
 
-        [Option('w', "password", Required = true, HelpText = "Password")]
-        public string? Password { get; set; }
+    [Option('w', "password", Required = true, HelpText = "Password")]
+    public string? Password { get; set; }
 
-        [Option('l', "url", Required = true, HelpText = "Uniform Resource Locator")]
-        public string? URL { get; set; }
+    [Option('l', "url", Required = true, HelpText = "Uniform Resource Locator")]
+    public string? URL { get; set; }
 
-        [Option('i', "index", Required = false, HelpText = "Index")]
-        public string? Index { get; set; }
+    [Option('i', "index", Required = false, HelpText = "Index")]
+    public string? Index { get; set; }
 
-        //[Option('s', "search", Required = false, HelpText = "Search")]
-        //public bool Search { get; set; }
-    }
+    [Option('z', "availability", Required = false, HelpText = "Availability")]
+    public bool Availability { get; set; }
+
+    //[Option('s', "search", Required = false, HelpText = "Search")]
+    //public bool Search { get; set; }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -11,61 +11,8 @@ internal class Program
 {
     static async Task Main(string[] args)
     {
-        // Testiranje stanja indeksa uz argumente
-        // Primer: dotnet run "http://10.7.7.2:9200/" "elastic" "DwzU0n-AD2RYuOd3MwZV" "dt_prilozi"
-
-        //var settings = new ConnectionSettings(new Uri(args[0])
-        //    .BasicAuthentication(args[1], args[2]);
-
-        //var client = new ElasticClient(settings);
-
-        //var response = client.Cat.Indices(descriptor => descriptor.Index(args[3]));
-
-        //if (response.IsValid)
-        //{
-        //    foreach (var index in response.Records)
-        //    {
-        //        Console.WriteLine($"Index: {index.Index}, Health: {index.Health}, Status: {index.Status}, Docs count: {index.DocsCount}");
-        //    }
-        //}
-        //else
-        //{
-        //    Console.WriteLine($"Error: {response.OriginalException}");
-        //}
-
-        // Provera dostupnosti ES
-
-        //var elasticUrl = "http://10.7.7.2:9200/";
-        //var elasticUsername = "elastic";
-        //var elasticPassword = "DwzU0n-AD2RYuOd3MwZV";
-
-        //var httpClient = new HttpClient();
-
-        //var base64Credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{elasticUsername}:{elasticPassword}"));
-        //httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64Credentials);
-
-        //try
-        //{
-        //    var response = await httpClient.GetAsync(elasticUrl);
-
-        //    if (response.IsSuccessStatusCode)
-        //    {
-        //        Console.WriteLine("Elasticsearch is available");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine($"Failed to connect to Elasticsearch. Status code: {response.StatusCode}");
-        //    }
-        //}
-        //catch (Exception ex)
-        //{
-        //    Console.WriteLine($"An error occurred while connecting to Elasticsearch: {ex.Message}");
-        //}
-
-        // Opcije
-
         Parser.Default.ParseArguments<EsOptions>(args)
-            .WithParsed(opts =>
+            .WithParsed(async opts =>
             {
                 var settings = new ConnectionSettings(new Uri(opts.URL))
                     .BasicAuthentication(opts.Username, opts.Password);
@@ -73,6 +20,34 @@ internal class Program
                 var client = new ElasticClient(settings);
 
                 var response = client.Cat.Indices(descriptor => descriptor.Index(opts.Index));
+
+                if (opts.Availability)
+                {
+                    Console.WriteLine("Checking Elasticsearch availability...");
+
+                    //var httpClient = new HttpClient();
+
+                    //var base64Credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{opts.Username}:{opts.Password}"));
+                    //httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64Credentials);
+
+                    //try
+                    //{
+                    //    var availabilityResponse = await httpClient.GetAsync(opts.URL);
+
+                    //    if (availabilityResponse.IsSuccessStatusCode)
+                    //    {
+                    //        Console.WriteLine("Elasticsearch is available");
+                    //    }
+                    //    else
+                    //    {
+                    //        Console.WriteLine($"Failed to connect to Elasticsearch. Status code: {availabilityResponse.StatusCode}");
+                    //    }
+                    //}
+                    //catch (Exception ex)
+                    //{
+                    //    Console.WriteLine($"An error occurred while connecting to Elasticsearch: {ex.Message}");
+                    //}
+                }
 
                 if (response.IsValid)
                 {
@@ -85,34 +60,7 @@ internal class Program
                 {
                     Console.WriteLine($"Error: {response.OriginalException}");
                 }
-
-                //    if (opts.Name != null)
-                //    {
-                //        if (opts.Insert)
-                //        {
-                //            var response = client.IndexDocument(new { Name = opts.Name, Age = opts.Age });
-                //            Console.WriteLine("Indexed");
-                //        }
-                //        else if (opts.Search)
-                //        {
-                //            Console.WriteLine($"Option is {opts.Search} I'll try to search");
-
-                //            var searchResponse = client.Search<Person>(s => s
-                //            .Query(q => q
-                //                .Match(m => m
-                //                    .Field(f => f.Name)
-                //                        .Query(opts.Name))));
-
-                //            Console.WriteLine(("Results"));
-
-                //            foreach (var hit in searchResponse.Hits)
-                //            {
-                //                Console.WriteLine($"Name> {hit.Source.Name} --- Age:{hit.Source.Age} --- Index:{hit.Index} --- ES Id:{hit.Id}");
-                //            }
-                //        }
-                //    }
-                //})
-                //.WithNotParsed(errs => Console.WriteLine(errs.ToString()));
-            });
+            })
+            .WithNotParsed(errs => Console.WriteLine(errs.ToString()));
     }
 }

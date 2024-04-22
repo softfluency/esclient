f020515b2a750ab481c938404843ffceb63fecdc
dejan <dsindjelic@live.com>
2024-04-22 12:14:17 +0200

    returning server status when sending url

--- src/Program.cs
+++ src/Program.cs
@@ -25,26 +25,41 @@ internal class Program
 
                 var client = new ElasticClient(conn);
 
-                var response = client.Cat.Indices(descriptor => descriptor.Index(opts.Index));
+                if (opts.URL != null)
+                {
+                    var resp = client.Ping();
+                    if (resp.IsValid)
+                    {
+                        Console.WriteLine("All is well");
+                    }
+                    else
+                    {
+                        Console.WriteLine(resp.ServerError);
+                        Console.WriteLine(resp.OriginalException.ToString());
+                        Console.WriteLine("Elasticsearch cluster is down");
+                    }
+                }
 
-                if (response.IsValid && opts.Index != null)
-                {
-                    foreach (var index in response.Records)
-                    {
-                        Console.WriteLine($"Index: {index.Index},\nHealth: {index.Health},\nStatus: {index.Status},\nDocs count: {index.DocsCount},\nDeleted: {index.DocsDeleted},\nStore size: {index.StoreSize}");
-                    }
-                }
-                else if (response.IsValid)
-                {
-                    foreach (var index in response.Records)
-                    {
-                        Console.WriteLine($"Index: {index.Index}, Health: {index.Health}, Status: {index.Status}, Docs count: {index.DocsCount}");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Error: {response.OriginalException}");
-                }
+                //var response = client.Cat.Indices(descriptor => descriptor.Index(opts.Index));
+
+                //if (response.IsValid && opts.Index != null)
+                //{
+                //    foreach (var index in response.Records)
+                //    {
+                //        Console.WriteLine($"Index: {index.Index},\nHealth: {index.Health},\nStatus: {index.Status},\nDocs count: {index.DocsCount},\nDeleted: {index.DocsDeleted},\nStore size: {index.StoreSize}");
+                //    }
+                //}
+                //else if (response.IsValid)
+                //{
+                //    foreach (var index in response.Records)
+                //    {
+                //        Console.WriteLine($"Index: {index.Index}, Health: {index.Health}, Status: {index.Status}, Docs count: {index.DocsCount}");
+                //    }
+                //}
+                //else
+                //{
+                //    Console.WriteLine($"Error: {response.OriginalException}");
+                //}
             })
             .WithNotParsed(errs => Console.WriteLine(errs.ToString()));
     }

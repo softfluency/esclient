7eb5fdb50f85d7c2274b8d9e33344c75a410e3c7
MiloBg <milofcrs@gmail.com>
2024-05-07 12:27:12 +0200

    Initial code improvements

--- src/Program.cs
+++ src/Program.cs
@@ -4,66 +4,61 @@ using Nest;
 
 namespace esclient;
 
-internal class Program
+class Program
 {
     static void Main(string[] args)
     {
         Parser.Default.ParseArguments<EsOptions>(args)
             .WithParsed(opts =>
             {
-                var esUrl = opts.URL;
-
-                if (esUrl != null)
-                {
-                    var server = new Uri(esUrl);
-
-                    var conn = new ConnectionSettings(server);
-
-                    conn.EnableHttpCompression();
-                    conn.ConnectionLimit(-1);
-
-                    var client = new ElasticClient(conn);
-                    var response = client.Cat.Indices(descriptor => descriptor.Index(opts.Index));
-
-                    if (response.IsValid && opts.Index == null && args.Length == 3)
-                    {
-                        var indexesTable = new ConsoleTable("Index", "Health", "Status");
-                        foreach (var index in response.Records)
-                        {
-                            indexesTable.AddRow(index.Index, index.Health, index.Status);
-                        }
-                        indexesTable.Write();
-                    }
-                    else if (response.IsValid && opts.Index != null)
-                    {
-                        var indexTable = new ConsoleTable("Index", "Health", "Status", "Docs count", "Deleted", "Store size");
-                        indexTable.Options.EnableCount = false;
-                        foreach (var index in response.Records)
-                        {
-                            indexTable.AddRow(index.Index, index.Health, index.Status, index.DocsCount, index.DocsDeleted, index.StoreSize);
-                        }
-                        indexTable.Write();
-                    }
-                    else if (response.IsValid && opts.Index == null)
-                    {
-                        var resp = client.Ping();
-                        if (resp.IsValid)
-                        {
-                            Console.WriteLine("Elasticsearch cluster is up and running");
-                        }
-                        else
-                        {
-                            Console.WriteLine(resp.ServerError);
-                            Console.WriteLine(resp.OriginalException.ToString());
-                            Console.WriteLine("Elasticsearch cluster is down");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Error: {response.OriginalException}");
-                    }
-                }
+                OptionsParse(args, opts);
             })
-            .WithNotParsed(errs => Console.WriteLine(errs.ToString()));
+            .WithNotParsed(errs => Console.Write(""));
+    }
+
+    private static void OptionsParse(string[] args, EsOptions opts)
+    {
+        var server = new Uri(opts.URL);
+
+        var conn = new ConnectionSettings(server);
+
+        conn.EnableHttpCompression();
+        conn.ConnectionLimit(-1);
+
+        var client = new ElasticClient(conn);
+        var response = client.Cat.Indices(descriptor => descriptor.Index(opts.Index));
+
+        if (!response.IsValid)
+        {
+            Console.WriteLine($"Error: {response.OriginalException}");
+            return;
+        }
+
+        if (opts.Index == null && args.Length == 3)
+        {
+            // -l URL -i Print all indexes
+            var indexesTable = new ConsoleTable("Index", "Health", "Status");
+            foreach (var index in response.Records)
+            {
+                indexesTable.AddRow(index.Index, index.Health, index.Status);
+            }
+            indexesTable.Write();
+        }
+        else if (opts.Index != null)
+        {
+            // -l URL -i INDEX Print one index
+            var indexTable = new ConsoleTable("Index", "Health", "Status", "Docs count", "Deleted", "Store size");
+            indexTable.Options.EnableCount = false;
+            foreach (var index in response.Records)
+            {
+                indexTable.AddRow(index.Index, index.Health, index.Status, index.DocsCount, index.DocsDeleted, index.StoreSize);
+            }
+            indexTable.Write();
+        }
+        else
+        {
+            // -l URL Status
+            Console.WriteLine("Elasticsearch cluster is up and running");
+        }
     }
 }

4d9f87f8343702a5006dd6dd928145c4ad4ab2ca
dejan <dsindjelic@live.com>
2024-05-08 11:03:02 +0200

    Created Classes

---
+++ src/ElasticClientFactory.cs
@@ -0,0 +1,24 @@
+﻿using Nest;
+
+namespace esclient
+{
+    public class ElasticClientFactory
+    {
+        public ElasticClient CreateClient(ConnectionSettings settings)
+        {
+            var connectionSettings = new Nest.ConnectionSettings(settings.ServerUri);
+
+            if (settings.HttpCompressionEnabled)
+            {
+                connectionSettings.EnableHttpCompression();
+            }
+
+            if (settings.ConnectionLimitValue != 0)
+            {
+                connectionSettings.ConnectionLimit(settings.ConnectionLimitValue);
+            }
+
+            return new ElasticClient(connectionSettings);
+        }
+    }
+}

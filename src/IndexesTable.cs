271d0e0678b6ae07b40043745766198fb59f4b05
dejan <dsindjelic@live.com>
2024-05-08 14:04:16 +0200

    Extracted IndexesTable class

---
+++ src/IndexesTable.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleTables;
+
+namespace esclient
+{
+    public class IndexesTable
+    {
+        public static ConsoleTable CreateTable(params string[] headers)
+        {
+            var table = new ConsoleTable(headers);
+
+            return table; 
+        }
+    
+        public static void AddRow(ConsoleTable table, params object[] values)
+        {
+            table.AddRow(values);
+        }
+    }
+}
+

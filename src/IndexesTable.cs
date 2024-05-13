a71bc49e78c9007338dc25278c2c02a3e55302a0
MiloBg <milofcrs@gmail.com>
2024-05-13 11:42:56 +0200

    Improved table creating

--- src/IndexesTable.cs
+++ src/IndexesTable.cs
@@ -1,19 +1,30 @@
 ﻿using ConsoleTables;
+using Nest;
 
 namespace esclient
 {
     public class IndexesTable
     {
-        public static ConsoleTable CreateTable(params string[] headers)
+        public static void PrintTable(string[] headers, params object[][] rows)
         {
             var table = new ConsoleTable(headers);
-
-            return table; 
+            foreach (var row in rows)
+            {
+                table.AddRow(row);
+            }
+            table.Write();
         }
-    
-        public static void AddRow(ConsoleTable table, params object[] values)
+
+        public static void PrintAllIndices(string[] headers, CatResponse<CatIndicesRecord> response)
         {
-            table.AddRow(values);
+            var rows = response.Records.Select(index => new object[] { index.Index, index.Health, index.Status }).ToArray();
+            PrintTable(headers, rows);
+        }
+
+        public static void PrintSingleIndex(string[] headers, CatResponse<CatIndicesRecord> response)
+        {
+            var rows = response.Records.Select(index => new object[] { index.Index, index.Health, index.Status, index.DocsCount, index.DocsDeleted, index.StoreSize }).ToArray();
+            PrintTable(headers, rows);
         }
     }
 }

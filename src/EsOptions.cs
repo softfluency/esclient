9a05f76f1107f189dfb9920136859eedae935c9d
MiloBg <milofcrs@gmail.com>
2024-05-14 11:21:50 +0200

    Extracted all the verbs to EsOptions

--- src/EsOptions.cs
+++ src/EsOptions.cs
@@ -4,9 +4,27 @@ namespace esclient;
 
 public class EsOptions
 {
-    [Option('l', "url", Required = true, HelpText = "Uniform Resource Locator")]
-    public required string URL { get; set; }
+    [Verb("status", HelpText = "Returns the status of Elasticsearch.")]
+    public class Status
+    {
+        [Option('u', "url", HelpText = "Uniform Resource Locator")]
+        public required string URL { get; set; }
+    }
 
-    [Option('i', "index", Required = false, HelpText = "Index")]
-    public string? Index { get; set; }
+    [Verb("indices", HelpText = "Returns all of the indices.")]
+    public class Indices
+    {
+        [Option('u', "url", HelpText = "Uniform Resource Locator")]
+        public required string URL { get; set; }
+    }
+
+    [Verb("index", HelpText = "Returns entered index.")]
+    public class Index
+    {
+        [Option('u', "url", HelpText = "Uniform Resource Locator")]
+        public required string URL { get; set; }
+
+        [Option('i', "index", Required = false, HelpText = "Index")]
+        public required string IndexSearch { get; set; }
+    }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,55 +1,39 @@
 ﻿using CommandLine;
 using esclient.Elastic;
-using Nest;
 
 namespace esclient;
-
-[Verb("status", HelpText = "Returns the status of Elasticsearch.")]
-class Status
-{
-    [Option('u', "url", HelpText = "Uniform Resource Locator")]
-    public required string URL { get; set; }
-}
-
-[Verb("indices", HelpText = "Returns all of the indices.")]
-class Indices
-{
-    [Option('u', "url", HelpText = "Uniform Resource Locator")]
-    public required string URL { get; set; }
-}
-
-[Verb("index", HelpText = "Returns entered index.")]
-class Index
-{
-    [Option('u', "url", HelpText = "Uniform Resource Locator")]
-    public required string URL { get; set; }
-
-    [Option('i', "index", Required = false, HelpText = "Index")]
-    public required string IndexSearch { get; set; }
-}
 
 class Program
 {
     static int Main(string[] args)
     {
-        return Parser.Default.ParseArguments<Status, Indices, Index>(args)
+        return Parser.Default.ParseArguments<EsOptions.Status, EsOptions.Indices, EsOptions.Index>(args)
             .MapResult(
-                (Status opts) => ReturnStatus(opts),
-                (Indices opts) => ReturnIndices(opts),
-                (Index opts) => ReturnIndex(opts),
+                (EsOptions.Status opts) => ReturnStatus(opts),
+                (EsOptions.Indices opts) => ReturnIndices(opts),
+                (EsOptions.Index opts) => ReturnIndex(opts),
                 errs => 1);
     }
 
-    public static int ReturnStatus(Status opts)
+    public static int ReturnStatus(EsOptions.Status opts)
     {
         var elasticsearchService = new ElasticsearchService(opts.URL);
         var status = elasticsearchService.GetServerStatus();
-        Console.WriteLine(status);
-        Console.WriteLine(opts.URL);
+
+        if (status.IsValid)
+        {
+            Console.WriteLine("Elasticsearch cluster is up and running");
+            Console.WriteLine(status);
+        }
+        else
+        {
+            Console.WriteLine(status.ServerError.ToString());
+        }
+
         return 0;
     }
 
-    public static int ReturnIndices(Indices opts)
+    public static int ReturnIndices(EsOptions.Indices opts)
     {
         var elasticsearchService = new ElasticsearchService(opts.URL);
         var response = elasticsearchService.GetIndices();
@@ -60,7 +44,7 @@ class Program
         return 0;
     }
 
-    public static int ReturnIndex(Index opts)
+    public static int ReturnIndex(EsOptions.Index opts)
     {
         var elasticsearchService = new ElasticsearchService(opts.URL);
         var response = elasticsearchService.GetIndex(opts.IndexSearch);
@@ -69,47 +53,5 @@ class Program
         IndexesTable.PrintSingleIndex(headers, response);
 
         return 0;
-    }
-
-    private static void OptionsParse(EsOptions opts)
-    {
-        var elasticsearchService = new ElasticsearchService(opts.URL);
-
-        var status = elasticsearchService.GetServerStatus();
-        var isValid = status.IsValid;
-        if (isValid)
-        {
-            Console.WriteLine("Elasticsearch cluster is up and running");
-        }
-        else
-        {
-            Console.WriteLine(status.ServerError);
-        }
-
-        var response = opts.Index == null 
-            ? elasticsearchService.GetIndices() 
-            : elasticsearchService.GetIndex(opts.Index);
-
-        if (!response.IsValid)
-        {
-            Console.WriteLine($"Error: {response.OriginalException}");
-            return;
-        }
-
-        if (opts.Index == null)
-        {
-            string[] headers = { "Index", "Health", "Status" };
-            IndexesTable.PrintAllIndices(headers, response);
-        }
-        else if (opts.Index != null)
-        {
-            string[] headers = { "Index", "Health", "Status", "Docs count", "Deleted", "Store size" };
-            IndexesTable.PrintSingleIndex(headers, response);
-        }
-        else
-        {
-            // -l URL Status
-            Console.WriteLine("Elasticsearch cluster is up and running");
-        }
     }
 }
